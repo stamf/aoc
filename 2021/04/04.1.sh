976ff7b53039182409892eb171ff0a5e3d8a5885
stamf <ant@stamfel.com>
2021-12-04 17:25:38 +1100

    puzzle 04

---
+++ 2021/04/04.1.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+rm xx*.* 2>/dev/null
+
+csplit -s --suppress-matched input '/^$/' '{*}'
+
+sed 's/,/\n/g' xx00 > numbers
+rm -f xx00
+
+createLines() {
+  for((i = 1; i <= 5; i++)) {
+    sed -i 's/ \+/ /g;s/^ //' "$1"
+    sed -nE "${i}s/ /\n/gp"   "$1" > "${1}.r${i}"
+    cut -f"$i" -d' '          "$1" > "${1}.c${i}"
+  }
+}
+
+for board in xx*; do createLines "$board"; done
+
+for((i = 1; i <= $(wc -l < numbers); i++)) {
+  grep -c -wF -f <(head -n $i numbers) xx*.* \
+    | awk -F: -v i=$i '$2 == 5 { print i, $1 }'
+} | sed 's/\..*$//' \
+  | head -n 1 \
+  | ( read line board;
+
+      a=$(sed -n "${line}p" numbers);
+      b=$(sort -u ${board}.* \
+        | grep -vwFf <(head -n $line numbers) \
+        | awk '{n += $1} END {print n}')
+
+      echo $a $b $((a * b))
+    )
