a515148ad34bff453b0846484a6dc083e42267e0
stamf <ant@stamfel.com>
2021-12-06 21:38:54 +1100

    puzzle 06

---
+++ 2021/06/06.2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sed 's/,/\n/g' input \
+  | awk -v n=256 '
+      { x[$1]++; }
+
+      END {
+        for(i = 0; i < n; i++) {
+          x[i + 7] += x[i];
+          x[i + 9] += x[i];
+        }
+        for(; i < length(x); i++) {
+          z += x[i];
+        }
+        print z;
+      }
+    '
